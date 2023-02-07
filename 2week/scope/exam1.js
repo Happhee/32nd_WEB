1dcd4da0743be720cb7f6f8d327ac7d7c72f9f5e
seohee0112 <ghdtj7@naver.com>
2023-02-07 16:37:32 +0900

    feat : 스코프 예제1 생성

---
+++ 2week/scope/exam1.js
@@ -0,0 +1,11 @@
+for (let letNum = 0; letNum < 10; letNum++) {
+  console.log("let", letNum);
+}
+  console.log("let", letNum);
+
+for (var varNum = 0; varNum < 10; varNum++){
+    console.log('var', varNum);
+}
+    console.log("var", varNum);
+
+  
