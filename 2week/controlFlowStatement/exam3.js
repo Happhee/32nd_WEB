65d3e7939edd26121e26b0379d18db957fd02b49
seohee0112 <ghdtj7@naver.com>
2023-02-09 14:44:02 +0900

    feat : switch 문 예쩨

---
+++ 2week/controlFlowStatement/exam3.js
@@ -0,0 +1,21 @@
+function mtInfo(mtMember) {
+    switch (mtMember) {
+        case 23:
+            console.log('앗싸앗싸!!!!!!');
+            break;
+        case 22:
+            console.log('어어어...?');
+           
+        case 21:
+            console.log('...🥺 왜왜왱 ㅠㅠ');
+            break;
+        case 20:
+            console.log('집착하기 시작..>.<');
+            break;
+            defalut:
+            console.log('가서 햅히하게 놀아부쟈!!!');
+            break;
+    }
+}
+console.log(mtInfo(23));
+console.log(mtInfo(22));
