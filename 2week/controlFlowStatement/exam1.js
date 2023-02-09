933528f4fab58c4e1a04fad6570bea5a436593a0
seohee0112 <ghdtj7@naver.com>
2023-02-09 14:10:05 +0900

    feat : 블록문 구현

---
+++ 2week/controlFlowStatement/exam1.js
@@ -0,0 +1,18 @@
+// 블록문
+{
+  let hungry = "밥주세요!!!";
+  console.log(hungry);
+}
+
+// 제어문
+let num = 0;
+while (num < 12) {
+  num++;
+}
+console.log(num);
+
+// 함수 선언문
+function webPart(happhee, web) {
+  return happhee + web;
+}
+console.log(webPart("웹파트장", " 파트원들 없으면 안데!!!!"));
