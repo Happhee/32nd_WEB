51d6cc92837892192c279f0d9f7a49a19557dd39
seohee0112 <ghdtj7@naver.com>
2023-02-09 18:31:14 +0900

    feat : 객체 키로 접근하기

---
+++ 2week/object/exam2.js
@@ -0,0 +1,12 @@
+const webPart = {
+  leader: "Happhee",
+  vision: "긍정과 열정이 넘치는 웹개발자들",
+  "like webMt": true,
+};
+
+const webPartKey = "like webMt";
+
+console.log(webPart[webPartKey]);
+console.log(webPart["like webMt"]);
+console.log(webPart.webPartKey);
+// console.log(webPart."like webMt");
