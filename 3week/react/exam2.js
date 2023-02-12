94cfbbe1e8e2cb81d6bb00f7669465be73d92bc4
seohee0112 <ghdtj7@naver.com>
2023-02-12 23:06:41 +0900

    feat : JS JSX 비교하기

---
+++ 3week/react/exam2.js
@@ -0,0 +1,12 @@
+//자바스크립트
+function getJSTitle() {
+  return React.createElement("h1", null, "Happhee Web Part 💛💛 ");
+}
+
+//JSX
+function getJSXTitle() {
+  return (
+    <h1>Happhee Web Part 💛💛</h1>
+  );
+
+console.log(getJSTitle());
