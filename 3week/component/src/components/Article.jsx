112128c5f38edbba24445e67006634478fff1bb8
seohee0112 <ghdtj7@naver.com>
2023-02-14 12:10:02 +0900

    feat : article 컴포넌트 구현

---
+++ 3week/component/src/components/Article.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+class Article extends Component {
+  render() {
+    return (
+      <article>
+        <header>
+          <h2>오늘은 3주차 </h2>
+        </header>
+        <p>React 같이 뿌셔보기🤔</p>
+      </article>
+    );
+  }
+}
+export default Article;
