3fe148ebc3a5f68885f0db818a2864be20674968
seohee0112 <ghdtj7@naver.com>
2023-02-14 12:13:11 +0900

    feat : 컴포넌트 불러와서 app렌더링

---
+++ 3week/component/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Article, Child, Component, Header, WebPartList } from "./components";
+
+const App = () => {
+  return (
+    <div>
+      <Component />
+    </div>
+  );
+};
+
+export default App;
--- 3week/component/src/components/index.js
+++ 3week/component/src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Article } from "./Article";
 export { default as Header } from "./Header";
 export { default as WebPartList } from "./WebPartList";
+export { default as Child } from "./Child";
+export { default as Component } from "./Component";
