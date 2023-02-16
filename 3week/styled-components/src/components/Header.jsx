6964e19666755f5d888dd5ce4ed72572f6395963
seohee0112 <ghdtj7@naver.com>
2023-02-16 15:54:42 +0900

    feat : 헤더 컴포넌트 만들기

---
+++ 3week/styled-components/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+const Header = () => {
+  return <HappheeHeader>💛 햅히의 웹파트 💛</HappheeHeader>;
+};
+
+export default Header;
+
+const HappheeHeader = styled.header`
+  width: 50%;
+  margin: 0;
+
+  background-color: #ff8e9e;
+  font-size: 2rem;
+`;
