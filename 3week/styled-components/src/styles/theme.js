21e04651e09136631edf9278ec080e8bb9df0e17
seohee0112 <ghdtj7@naver.com>
2023-02-17 15:19:51 +0900

    style : theme color 적용하기

---
+++ 3week/styled-components/src/styles/theme.js
@@ -0,0 +1,15 @@
+const colors = {
+  white: "#FFFFFF",
+  black: "#000000",
+  skyBlue: "#C7F5FE",
+  lightPink: "#FCC8F8",
+  lightPurple: "#EAB4F8",
+  purple: "#9900F0",
+  lightYellow: "#F3F798",
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
