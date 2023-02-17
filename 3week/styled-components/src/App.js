14bbef61eb0f1c084fc68965286c02dc883e203a
seohee0112 <ghdtj7@naver.com>
2023-02-17 14:05:59 +0900

    feat : styled-components 미디어 쿼리 생성

--- 3week/styled-components/src/App.js
+++ 3week/styled-components/src/App.js
@@ -1,11 +1,32 @@
-import { Header } from "./components";
+import styled from "styled-components";
+import { Button, Header } from "./components";
+import GlobalStyle from "./styles/globalStyleds";
 
 const App = () => {
   return (
-    <>
+    <AppWrapper>
+      <GlobalStyle />
       <Header />
-    </>
+      <Button />
+    </AppWrapper>
   );
 };
 
 export default App;
+
+const AppWrapper = styled.div`
+  display: flex;
+
+  width: 50rem;
+  margin: 0 auto;
+
+  background-color: red;
+  @media (max-width: 50rem) {
+    width: 30rem;
+    background-color: yellow;
+  }
+  @media (max-width: 30rem) {
+    width: 100%;
+    background-color: green;
+  }
+`;
