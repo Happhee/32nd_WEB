8e5168bb091158c2957224de32014a216186a9ad
seohee0112 <ghdtj7@naver.com>
2023-05-03 17:17:05 +0900

    init : react-router-dom 초기 설정

---
+++ week4/react-router-dom/src/App.jsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import Router from "./Router";
+
+import GlobalStyle from "./styles/globalStyle";
+import theme from "./styles/theme";
+
+const App = () => (
+  <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router />
+    </ThemeProvider>
+  </>
+);
+
+export default App;
