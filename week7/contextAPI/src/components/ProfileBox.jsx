279f3a44ad35c5e34e60100bca4c4976c0d50c3b
seohee0112 <ghdtj7@naver.com>
2023-05-25 14:09:15 +0900

    feat : action이 정의되어있는 Consumer 구현

--- week7/contextAPI/src/components/ProfileBox.jsx
+++ week7/contextAPI/src/components/ProfileBox.jsx
@@ -1,15 +1,22 @@
-import ProfileContext from "../contexts/Profile";
-
+import ProfileActionContext from "../contexts/ProfileActionContext";
 const ProfileBox = () => {
   return (
-    <ProfileContext.Consumer>
-      {({ name, age }) => (
-        <div>
-          <p>이름 : {name}</p>
-          <p>나이 : {age}</p>
-        </div>
+    <ProfileActionContext.Consumer>
+      {({ state: { name, age }, actions: { setAge, setName } }) => (
+        <>
+          <div>
+            <p>이름 : {name}</p>
+            <p>나이 : {age}</p>
+          </div>
+          <button type="button" onClick={() => setName("서히")}>
+            서히로 바꾸기
+          </button>
+          <button type="button" onClick={() => setAge((prev) => prev + 1)}>
+            나이 증가
+          </button>
+        </>
       )}
-    </ProfileContext.Consumer>
+    </ProfileActionContext.Consumer>
   );
 };
 
