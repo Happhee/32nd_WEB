856dee02606b087195eba01bafbd157cb81e732c
seohee0112 <ghdtj7@naver.com>
2023-05-25 14:09:44 +0900

    feat : value, action을 나눠서 컴포넌트 구현

--- week7/contextAPI/src/App.jsx
+++ week7/contextAPI/src/App.jsx
@@ -1,9 +1,17 @@
+import ProfileBox from "./components/ProfileBox";
 import Profile from "./consumer/Profile";
+import ProfileProvider from "./providers/Profile";
 
 const App = () => {
   return (
     <div>
+      <h1>프로필만 보여주기</h1>
       <Profile />
+      <hr />
+      <h1>액션이 있는 프로필</h1>
+      <ProfileProvider>
+        <ProfileBox />
+      </ProfileProvider>
     </div>
   );
 };
