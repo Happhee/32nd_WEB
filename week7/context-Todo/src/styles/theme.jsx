5cf8b5277c4fe53c1d0a4a24f3d3bdae4dd4faa1
seohee0112 <ghdtj7@naver.com>
2023-05-25 14:58:55 +0900

    style : 글로벌 스타일 정의

---
+++ week7/context-Todo/src/styles/theme.jsx
@@ -0,0 +1,15 @@
+const colors = {
+  white: "#FFFFFF",
+  black: "#000000",
+  skyBlue_1: "#C0DBEA",
+  pink_1: "#E8A0BF",
+  pink_2: "#FDF4F5",
+  purple_1: "#917FB3",
+  purple_2: "#BA90C6",
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
